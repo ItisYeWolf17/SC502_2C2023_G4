7f741b6a8ed2185c5e864b3607646a4ce9e7bc44
canntii <bryandc681@gmail.com>
2023-07-16 12:08:37 -0600

    Update login.js

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -3,9 +3,9 @@ let btnEnviar = document.querySelector(".btn-enviar");
 let lablCorreo = document.querySelector("#correo");
 let lblPassword = document.querySelector("#password");
 
-btnEnviar.addEventListener("click", function (event) {
-    event.preventDefault(); //Para evitar que la pagina parpadee cuando el btn es submit
-    if (lablCorreo.value == "" && lblPassword.value == "") {
+btnEnviar.addEventListener("click", function () {
+
+    if (lablCorreo.value == "" || lblPassword.value == "") {
         swal.fire({
             title: `Debes de completar los datos`,
             confirmButtonColor: '#0B3B59',
@@ -14,17 +14,5 @@ btnEnviar.addEventListener("click", function (event) {
                 popup: 'ventanaModal'
             }
         });
-        lblPassword.value = "";
-    } else {
-        console.log(lablCorreo.value);
-        swal.fire({
-            title: `Correo o clave incorrectas`,
-            confirmButtonColor: '#0B3B59',
-            icon: 'error',
-            customClass: {
-                popup: 'ventanaModal'
-            }
-        });
-        lblPassword.value = "";
-    }
+    } 
 })
